d9a86904631ba02f6953050d63a73af574d07644
jarczens <133011315+jarczens@users.noreply.github.com>
2023-06-04 04:07:20 +0200

    Update obwodkola.cs

--- obwodkola.cs
+++ obwodkola.cs
@@ -4,14 +4,17 @@ public class MyProgram
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Podaj pole kola");
+        Console.WriteLine("Podaj pole kola, aby obliczyc jego obwod");
         double poleKola;
         
         poleKola = inputValue();
+        double promienKola;
+        
+        promienKola = Math.PI * Math.Sqrt(poleKola);
         double obwodKola;
         
-        obwodKola = Math.Sqrt(poleKola);
-        Console.WriteLine(obwodKola);
+        obwodKola = 2 * Math.PI * promienKola;
+        Console.WriteLine("Obwod kola wynosi " + obwodKola + " ;)");
     }
     
     // .NET can only read single characters or entire lines from the
