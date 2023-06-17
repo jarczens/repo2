c6b49f7142238aa0260b2fab311fc4b4bca405b0
jarczens <133011315+jarczens@users.noreply.github.com>
2023-06-17 03:37:09 +0200

    Add files via upload

---
+++ 4_belka.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj kwotę, którą chcesz wpłacić na konto: ");
+        double kapitalPoczatkowy = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj oprocentowanie w skali roku: ");
+        double oprocentowanie = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj liczbę miesięcy oszczędzania: ");
+        int liczbaMiesiecy = int.Parse(Console.ReadLine());
+
+        double oprocentowanieMiesieczne = oprocentowanie / 12 / 100; 
+
+        double kapitalKoncowy = kapitalPoczatkowy * Math.Pow(1 + oprocentowanieMiesieczne, liczbaMiesiecy); 
+
+        double podatekBelki = 0.19 * (kapitalKoncowy - kapitalPoczatkowy); 
+
+        double kapitalKoncowyPoPodatku = kapitalKoncowy - podatekBelki; 
+
+        double kapitalKoncowyZaokraglony = Math.Round(kapitalKoncowyPoPodatku, 1); 
+
+        Console.WriteLine("Suma którą zaoszczędziłeś (po uwzględnieniu podatku Belki) to : " + kapitalKoncowyZaokraglony);
+        Console.ReadLine();
+    }
+}
---
+++ 5_szeregTaylora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static double Silnia(int liczba)
+    {
+        double wynik = 1;
+        for (int a = 2; a <= liczba; a++)
+        {
+            wynik *= a;
+        }
+        return wynik;
+    }
+
+    static double Funkcja(double x, int n)
+    {
+        double wynik = 0;
+        for (int a = 0; a <= n; a++)
+        {
+            double b = Math.Pow(x, a) / Silnia(a);
+            wynik += b; 
+        }
+        return wynik;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj ile wynosi x: ");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj liczbę wyrazów: ");
+        int n = int.Parse(Console.ReadLine());
+
+        double wartosc = Funkcja(x, n);
+
+        Console.WriteLine("Wartość funkcji e do potęgi " + x + " dla podanej ilości wyrazów wynosi: " + wartosc);
+        Console.ReadLine();
+    }
+}
